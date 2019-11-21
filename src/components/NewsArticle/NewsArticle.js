141b45e9c9e93893178c6cd6faf67ccdd5da941b
Alan Turing <austeyd@gmail.com>
2019-11-20 22:17:42 -0700

    Rename newsarticle img class

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -5,7 +5,7 @@ const NewsArticle = (props) => {
   return (
     <article className="news-article">
       <h3>{props.headline}</h3>
-      <img className="local-img" src={props.img} alt={props.headline}/>
+      <img className="news-img" src={props.img} alt={props.headline}/>
       <p>{props.description}</p>
       <a href={props.url}>{props.url}</a>
     </article>
