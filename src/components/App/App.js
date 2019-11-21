24176d110a34811c9807340a232fc64261651d6a
Alan Turing <austeyd@gmail.com>
2019-11-20 23:25:40 -0700

    Add searchArticlesByKeyword method to the app component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Menu from '../Menu/Menu';
 import NewsContainer from '../NewsContainer/NewsContainer';
+import SearchForm from '../SearchForm/SearchForm';
 import entertainment from '../../data/entertainment';
 import health from '../../data/health';
 import local from '../../data/local';
@@ -22,6 +23,13 @@ class App extends Component {
     })
   }
 
+  searchArticlesByKeyword = (searchQuery) => {
+    this.state.genreDisplayed.filter(newsStory => {
+      return newsStory.headline.toUppercase().includes(searchQuery.toUppercase())
+        || newsStory.description.toUppercase().includes(searchQuery.toUppercase())
+    })
+  }
+
   render () {
     return (
       <main className="app">
@@ -34,6 +42,7 @@ class App extends Component {
           technology={technology}
         />
         <NewsContainer genreDisplayed={this.state.genreDisplayed}/>
+        <SearchForm searchArticlesByKeyword={searchArticlesByKeyword}/>
       </main>
     );
   }
