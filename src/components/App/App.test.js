fd1a19b4a3e3736dff2d542cb23e33938a406a57
Alan Turing <austeyd@gmail.com>
2019-11-21 22:32:43 -0700

    Add test for switchStateGenre method

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -8,5 +8,17 @@ describe('App', () => {
     const wrapper = shallow(<App />);
 
     expect(wrapper).toMatchSnapshot();
-  })
+  });
+
+  it('should update state when switchStateGenre is called', () => {
+    const wrapper = shallow(<App />);
+    const mockGenre = [{mockProperty: 'MockValue'}];
+    const expected = mockGenre;
+
+    wrapper.instance().switchStateGenre(mockGenre);
+    expect(wrapper.state('genreDisplayed')).toEqual([{mockProperty: 'MockValue'}]);
+  });
+
+
+
 })
