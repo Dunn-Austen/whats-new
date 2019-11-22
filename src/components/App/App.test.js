a786248e60a75117886f92ac3cfe0c48bc0cbf6c
Alan Turing <austeyd@gmail.com>
2019-11-21 22:49:34 -0700

    Add test for searchArticlesByKeyword() method

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -16,9 +16,23 @@ describe('App', () => {
     const expected = mockGenre;
 
     wrapper.instance().switchStateGenre(mockGenre);
-    expect(wrapper.state('genreDisplayed')).toEqual([{mockProperty: 'MockValue'}]);
+    expect(wrapper.state('genreDisplayed')).toEqual(expected);
   });
 
+  it('should update state when searchArticlesByKeyword is called', () => {
+    const wrapper = shallow(<App />);
+    const mockSearchQuery = 'The Who';
 
+    wrapper.instance().searchArticlesByKeyword(mockSearchQuery);
+    expect(wrapper.state('genreDisplayed')).toEqual([
+      {
+      id: 1,
+      headline: 'The Who postpones Denver Concert at the Pepsi Center',
+      img: 'https://theknow.denverpost.com/wp-content/uploads/2016/03/CDXXTHEWHODSC_4082x.jpg',
+      description: 'Classic-rock band The Who has postponed its Denver concert, set for Sunday, Sept. 29, after an illness robbed singer Roger Daltrey of his voice, the band said on Twitter.',
+      url: 'https://theknow.denverpost.com/2019/09/27/the-who-postpones-denver-concert/225182/'
+      }
+    ]);
+  });
 
 })
