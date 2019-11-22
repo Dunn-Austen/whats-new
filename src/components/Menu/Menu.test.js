0acb6f656f276724e75627ea5455c6698c1e5bd4
Alan Turing <austeyd@gmail.com>
2019-11-21 23:42:31 -0700

    Add test to assess for switchStateGenre() invocation

--- src/components/Menu/Menu.test.js
+++ src/components/Menu/Menu.test.js
@@ -3,9 +3,10 @@ import Menu from './Menu';
 import { shallow } from 'enzyme';
 
 describe('Menu', () => {
+  let wrapper;
 
-  it('should match the snapshot with all data correctly passed', () => {
-    const wrapper = shallow(<Menu
+  beforeEach(() => {
+    wrapper = shallow(<Menu
       entertainment={[{id: 1, headline: 'A' , img: 'B' , description: 'C', url: 'D'}]}
       health={[{id: 2, headline: 'A' , img: 'B' , description: 'C', url: 'D'}]}
       local={[{id: 3, headline: 'A' , img: 'B' , description: 'C', url: 'D'}]}
@@ -13,7 +14,16 @@ describe('Menu', () => {
       switchStateGenre={jest.fn()}
       technology={[{id: 5, headline: 'A' , img: 'B' , description: 'C', url: 'D'}]}
       />);
+  });
 
+  it('should match the snapshot with all data correctly passed', () => {
     expect(wrapper).toMatchSnapshot();
-  })
+  });
+
+  it('should call switchStateGenre() upon button click', () => {
+    const abc = wrapper.switchStateGenre;
+
+    wrapper.find('#entertainment-btn').simulate('click');
+    expect(wrapper.switchStateGenre).toEqual(undefined);
+  });
 })
