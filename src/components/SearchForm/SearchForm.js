f7b15e2bdebd3cbf1f82f2e2eabe167b3d53073c
Alan Turing <austeyd@gmail.com>
2019-11-22 17:23:54 -0700

    Add searchForm styling

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -5,6 +5,7 @@ const SearchForm = (props) => {
 
   return(
     <section className="search-form">
+      <h4 className="search-text">Search Articles by Keyword:</h4>
       <div className="search-container">
         <input className="search-input" placeholder="Keyword"></input>
         <button className="search-btn" onClick={() => props.searchArticlesByKeyword(document.querySelector('.search-input').value)}>Search</button>
